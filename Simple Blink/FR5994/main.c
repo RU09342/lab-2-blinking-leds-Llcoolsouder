75ff99c36c749053e23299c1253499c87f9db316
Lonnie Souder II <souderl9@students.rowan.edu>
2017-10-02 11:57:23 -0400

    Added missing simple blink projects

---
+++ Simple Blink/FR5994/main.c
@@ -0,0 +1,30 @@
+/*
+ * main.c
+ *
+ *  Created on: Oct 2, 2017
+ *  Last Edited: Oct 2, 2017
+ *      Author: Lonnie Souder II
+ */
+
+#include <msp430.h> 
+
+#define LED_OUT P1OUT
+#define LED_DIR P1DIR
+
+
+int main(void)
+{
+	WDTCTL = WDTPW | WDTHOLD;	// Stop watchdog timer
+	
+    PM5CTL0 &= ~LOCKLPM5;       // Disable the GPIO power-on default high-impedance mode
+                                // to activate previously configured port settings
+
+	LED_DIR |= 0x0001;          // Set LED to output
+	LED_OUT &= 0x0000;          // Initialize LED to OFF
+
+	while (1){
+	    __delay_cycles(100000);
+	    LED_OUT ^= BIT0;        //Toggle LED
+	}
+
+}
